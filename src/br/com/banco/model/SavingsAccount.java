174c07fbe4469594f4c24e78d045082901d07a16
pauloepifani <paulorepifani@gmail.com>
2020-02-16 19:06:57 -0300

    new(SavingsAccount): create new account type

---
+++ src/br/com/banco/model/SavingsAccount.java
@@ -0,0 +1,9 @@
+package br.com.banco.model;
+
+public class SavingsAccount extends CheckingAccount {
+
+	public SavingsAccount(String number, Customer customer) {
+		super(number, customer);
+	}
+
+}

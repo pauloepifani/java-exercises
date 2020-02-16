e7a589395651a949a79511489fc41f5403643d4b
pauloepifani <paulorepifani@gmail.com>
2020-02-16 18:57:10 -0300

    refactor(BankMain): change class name

---
+++ src/br/com/banco/BankMain.java
@@ -0,0 +1,13 @@
+package br.com.banco;
+
+import br.com.banco.view.ApplicationMenu;
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationMenu.start();
+		
+	}
+
+}
